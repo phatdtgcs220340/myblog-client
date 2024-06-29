10d9f2af329664d4867112da8ce36775af699f0d
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-06-30 00:11:28 +0700

    add main component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { BodyComponent } from './layout/body/body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-client';
 }
+
