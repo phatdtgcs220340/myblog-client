7234974c7103272b7a122648afdf2d16e30de2de
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-02 23:14:19 +0700

    post card component

---
+++ src/app/components/post-card/post-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-card.component.html',
+  styleUrl: './post-card.component.css'
+})
+export class PostCardComponent {
+  @Input() title : string = ''
+}
