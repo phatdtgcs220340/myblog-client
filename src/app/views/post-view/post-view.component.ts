f18562cd4fc09dc73da8bec0660dee5d9e733363
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-02 23:15:31 +0700

    post view component init

---
+++ src/app/views/post-view/post-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-view.component.html',
+  styleUrl: './post-view.component.css'
+})
+export class PostViewComponent {
+
+}
