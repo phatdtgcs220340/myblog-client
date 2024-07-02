df03712c85d5424300414a6f010997919f5844d5
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-02 23:14:59 +0700

    add home view

--- src/app/layout/body/body.component.ts
+++ src/app/layout/body/body.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, output } from '@angular/core';
 import { LoginComponent } from '../../components/login/login.component';
+import { HomeViewComponent } from '../../views/home-view/home-view.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, HomeViewComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
-
+  @Input() displayLoginBox : boolean = false;
 }
