c38d350c35e728e81dc9474061c6e5c3d4629fc9
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-02 23:10:58 +0700

    move & rename interfaces

---
+++ src/app/api/interfaces/responses.interface.ts
@@ -0,0 +1,10 @@
+export interface PartialPost {
+  id : number,
+  title : string,
+  type : string,
+  dateAudit : string,
+  userId : number,
+  fullName : string,
+  totalLike : number,
+  totalReply : number
+}
