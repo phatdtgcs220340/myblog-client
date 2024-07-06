60e93232819f25979e17bdeea818f9589137d0c9
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-07 00:51:09 +0700

    add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { AuthorizeComponent } from './components/authorize/authorize.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path : 'home' , component : HomeViewComponent},
+  { path : 'authorized', component : AuthorizeComponent }
+];
