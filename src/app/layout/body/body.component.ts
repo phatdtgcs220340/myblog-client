add75de0c8043f5fbb834fa982bf0dd09465009a
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-07 00:50:57 +0700

    add router and view

--- src/app/layout/body/body.component.ts
+++ src/app/layout/body/body.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input, output } from '@angular/core';
 import { LoginComponent } from '../../shared/components/login/login.component';
 import { HomeViewComponent } from '../../views/home-view/home-view.component';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { PageNotFoundComponent } from '../../shared/components/page-not-found/page-not-found.component';
+import { AuthorizeComponent } from '../../components/authorize/authorize.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [LoginComponent, HomeViewComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, LoginComponent, HomeViewComponent, PageNotFoundComponent, AuthorizeComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
