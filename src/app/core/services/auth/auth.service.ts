05fc8394c028c4bada3b679ff7b22156bdceb8ac
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-07 00:50:02 +0700

    oauth2 logic

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,38 +1,27 @@
 import { Injectable } from '@angular/core';
 import { authorizationURL } from '../../../../app.env';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import { LoginForm } from '../../../shared/models/interfaces/requests.interface';
-import { TokenService } from '../token/token.service';
-
+import { Observable, throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apiUrl = `${authorizationURL}/api/login`
 
-  constructor(
-    private readonly http: HttpClient,
-    private readonly tokenService : TokenService
-  ) { }
+  constructor(private readonly http: HttpClient) {}
 
-  postData(form : LoginForm) : void {
+  postData(form : LoginForm) : Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type' : 'application/x-www-form-urlencoded'
     });
     const body = new URLSearchParams();
     body.set('username', form.username);
     body.set('password', form.password);
-
-    this.http.post<any>(this.apiUrl, body.toString(), {
+    return this.http.post<any>(this.apiUrl, body.toString(), {
       headers: headers,
-      withCredentials: true
-    }).subscribe({
-      next : response => {
-        console.log(response)
-      },
-      error : e => {
-        console.log(e)
-      }
-    });
+      withCredentials: true,
+      observe : 'response'
+    })
   }
 }
--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../core/services/auth/auth.service';
 import { LoginForm } from '../../models/interfaces/requests.interface';
 import { FormsModule } from '@angular/forms';
+import { TokenService } from '../../../core/services/token/token.service';
+import { authorizationLink } from '../../../../app.env';
 
 @Component({
   standalone : true,
@@ -17,15 +19,24 @@ export class LoginComponent implements OnInit {
     password : ''
   }
   isLoading : boolean = false
-
+  loginFailed : boolean = false
   constructor(private readonly service : AuthService) { }
 
   ngOnInit() {
   }
 
-  onFormSubmit() {
+  async onFormSubmit() {
     this.isLoading = true
-    this.service.postData(this.form)
-    setTimeout(() => this.isLoading = false, 2000)
+    const processLogin = this.service.postData(this.form).subscribe({
+      next : response => console.log(response),
+      error: error => {
+        const responseUrl : string = error.url
+        if (!responseUrl.includes("?error"))
+          console.log(authorizationLink)
+          window.location.assign(authorizationLink)
+      }
+    })
+    await setTimeout(() => processLogin, 2000)
+    this.isLoading = false
   }
 }
