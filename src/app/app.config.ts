3081e75567946ace90dd0bc8a2bd47ef837aa85c
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-07 23:17:42 +0700

    add store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { provideStore } from '@ngrx/store';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(CommonModule)
-  ],
+    importProvidersFrom(CommonModule),
+    provideStore()
+],
 };
