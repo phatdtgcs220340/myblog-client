aa559cd89cb7c185411675b6d3fbfdf8be25bce0
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-18 02:03:05 +0700

    upload post form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { PageNotFoundComponent } from './views/page-not-found/page-not-found.com
 import { HomeViewComponent } from './views/home-view/home-view.component';
 import { AuthorizeComponent } from './components/authorize/authorize.component';
 import { PostViewComponent } from './views/post-view/post-view.component';
+import { CreatePostViewComponent } from './views/create-post-view/create-post-view.component';
 
 export const routes: Routes = [
   { path : 'home', component : HomeViewComponent },
-  { path : 'authorized', component : AuthorizeComponent },
+  { path : 'authorize', component : AuthorizeComponent },
   { path : 'post/:id', component : PostViewComponent },
+  { path : 'admin' , component : CreatePostViewComponent },
   { path : '', redirectTo : 'home', pathMatch : "full" },
   { path : '**', component: PageNotFoundComponent }
 ];
