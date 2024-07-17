e342dbd798bd69216612ee2164032de176e50a72
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-18 02:02:11 +0700

    upload post form

--- src/app/views/create-post-view/create-post-view.component.ts
+++ src/app/views/create-post-view/create-post-view.component.ts
@@ -8,20 +8,20 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './create-post-view.component.html',
-  styleUrl: './create-post-view.component.css'
+  styleUrls: ['./create-post-view.component.css']
 })
 export class CreatePostViewComponent {
-  form : UploadPostForm = {
-    title : '',
-    type : 'THANH_HUYEN',
-    content : '',
-    files : new Array<ImageFile>()
-  }
-  isLoading : boolean = false
+  form: UploadPostForm = {
+    title: '',
+    type: 'THANH_HUYEN',
+    content: '',
+    files: new Array<ImageFile>()
+  };
+  isLoading: boolean = false;
 
   constructor(private readonly service: FetchPostsService) { }
 
-  displayBlob($event : Event) : void {
+  displayBlob($event: Event): void {
     const input = $event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
@@ -29,35 +29,44 @@ export class CreatePostViewComponent {
       reader.onload = () => {
         const arrayBuffer = reader.result as ArrayBuffer;
         const blob = new Blob([arrayBuffer], { type: file.type });
+        const newInput = this.cloneInputElement(input);
         this.form.files.push({
-          input : input,
-          blobUrl : URL.createObjectURL(blob)
-        })
+          input: newInput,
+          blobUrl: URL.createObjectURL(blob)
+        });
       };
       reader.readAsArrayBuffer(file);
     }
   }
 
-  async upload($event : Event) {
-    $event.preventDefault()
-    this.isLoading = true
-
-    const formData = new FormData()
-    formData.append('title', this.form.title)
-    formData.append('content', this.form.content)
-    formData.append('type', this.form.type)
-    await this.form.files.forEach(element => {
-      if (element.input.files != null && element.input.files.length > 0)
-        formData.append('files', element.input.files[0])
-    });
-    this.service.uploadForm(formData).subscribe({
-      next : response => response,
-      error : error => console.log(error)
-    })
-    setTimeout(() => this.isLoading = false, 2000)
+  cloneInputElement(input: HTMLInputElement): HTMLInputElement {
+    const clonedInput = document.createElement('input');
+    clonedInput.type = 'file';
+    clonedInput.files = input.files;
+    return clonedInput;
   }
 
-  deleteBlob(index : number) : void {
-    this.form.files.splice(index, 1)
+  async upload($event: Event) {
+    $event.preventDefault();
+    this.isLoading = true;
+    const formData = new FormData();
+    formData.append('title', this.form.title);
+    formData.append('content', this.form.content);
+    formData.append('type', this.form.type);
+    this.form.files.forEach(element => {
+      console.log(element.input.value)
+      if (element.input.files != null && element.input.files.length > 0) {
+        formData.append('files', element.input.files[0]);
+      }
+    });
+    await this.service.uploadForm(formData).subscribe({
+      next: response => response,
+      error: error => console.log(error)
+    });
+    this.isLoading = false
+  }
+
+  deleteBlob(index: number): void {
+    this.form.files.splice(index, 1);
   }
 }
