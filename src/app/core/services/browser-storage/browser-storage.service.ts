904e1fdf8a7372dde797f75d3251311e64884d27
lãng tử cam ranh <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-21 23:05:08 +0700

    session storage

--- src/app/core/services/browser-storage/browser-storage.service.ts
+++ src/app/core/services/browser-storage/browser-storage.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable, InjectionToken } from '@angular/core';
 export const BROWSER_STORAGE = new InjectionToken<Storage>('Browser Storage', {
   providedIn: 'root',
-  factory: () => localStorage
+  factory: () => sessionStorage
 });
 @Injectable({
   providedIn: 'root'
