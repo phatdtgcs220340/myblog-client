9bd6c117bdb68889801b2befa851a0d366e090a1
lãng tử cam ranh <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-24 23:31:07 +0700

    fetch data

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -1,13 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import { ImageCarouselComponent } from '../image-carousel/image-carousel.component';
+import { PartialPost } from '../../shared/models/interfaces/responses.interface';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-post-card',
   standalone: true,
-  imports: [ImageCarouselComponent],
+  imports: [ImageCarouselComponent, RouterLink],
   templateUrl: './post-card.component.html',
   styleUrl: './post-card.component.css'
 })
-export class PostCardComponent {
-  @Input() title : string = ''
+export class PostCardComponent implements AfterViewInit {
+  @Input() form : PartialPost = {
+    id: 0,
+    title: '',
+    type: '',
+    dateAudit: '',
+    images: []
+  }
+  link : string = ""
+
+  ngAfterViewInit(): void {
+    this.link = `/post/${this.form.id}`
+  }
 }
