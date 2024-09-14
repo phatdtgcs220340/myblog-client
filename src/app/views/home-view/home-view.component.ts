d7aa618ce7831aee1b0635b492238ca0b2393ba7
Phat Do <114555261+phatdtgcs220340@users.noreply.github.com>
2024-09-14 19:15:14 +0700

    update

--- src/app/views/home-view/home-view.component.ts
+++ src/app/views/home-view/home-view.component.ts
@@ -3,14 +3,13 @@ import { PostCardComponent } from '../../shared/components/post-card/post-card.c
 import { FetchPostsService } from '../../core/services/resources/posts/fetch-posts.service';
 import { PartialPost } from '../../shared/models/interfaces/responses.interface';
 import { LoadingCardComponent } from '../../shared/components/loading-card/loading-card.component';
-import { SideBarComponent } from '../../layout/side-bar/side-bar.component';
 import { RouterLink } from '@angular/router';
 import { BlogFilter } from '../../shared/models/interfaces/requests.interface';
 
 @Component({
   selector: 'app-home-view',
   standalone: true,
-  imports: [PostCardComponent, LoadingCardComponent, SideBarComponent, RouterLink],
+  imports: [PostCardComponent, LoadingCardComponent, RouterLink],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.css',
   providers : [FetchPostsService]
@@ -19,6 +18,7 @@ export class HomeViewComponent implements OnInit {
   posts : Array<PartialPost> = []
   serverDown : boolean = false
   isLoading : boolean = false
+  displayFilter : boolean = false
   filter : BlogFilter = {
     name: '',
     tags: [],
@@ -38,6 +38,10 @@ export class HomeViewComponent implements OnInit {
         this.isLoading = false
       }
     });
+  }
+
+  toggleDisplayFilter() {
+    this.displayFilter = !this.displayFilter
   }
 }
 
