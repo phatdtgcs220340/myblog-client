cb1538607a8b00348a1244c651738710f9c1b889
Phat Do <114555261+phatdtgcs220340@users.noreply.github.com>
2024-09-29 13:38:49 +0700

    remove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { BodyComponent } from './layout/body/body.component';
-import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, BodyComponent, FooterComponent],
+  imports: [NavbarComponent, BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
